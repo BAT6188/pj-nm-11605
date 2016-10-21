823fe679a59fdef9b6b2d58c9714f3956c8210ba
Administrator <kongxuhua@harmonywisdom.com>
2016-10-21 14:22:29 +0800

    建设项目环评及其他许可证情况模块

--- src/main/java/com/harmonywisdom/dshbcbp/composite/action/BuildProjectAction.java
+++ src/main/java/com/harmonywisdom/dshbcbp/composite/action/BuildProjectAction.java
@@ -1,16 +1,139 @@
 package com.harmonywisdom.dshbcbp.composite.action;
 
+import com.harmonywisdom.dshbcbp.attachment.service.AttachmentService;
+import com.harmonywisdom.dshbcbp.common.dict.util.DateUtil;
 import com.harmonywisdom.dshbcbp.composite.bean.BuildProject;
+import com.harmonywisdom.dshbcbp.composite.bean.ProjectAcceptance;
+import com.harmonywisdom.dshbcbp.composite.bean.ProjectEIA;
 import com.harmonywisdom.dshbcbp.composite.service.BuildProjectService;
+import com.harmonywisdom.dshbcbp.composite.service.ProjectAcceptanceService;
+import com.harmonywisdom.dshbcbp.composite.service.ProjectEIAService;
 import com.harmonywisdom.framework.action.BaseAction;
+import com.harmonywisdom.framework.dao.QueryCondition;
+import com.harmonywisdom.framework.dao.QueryOperator;
+import com.harmonywisdom.framework.dao.QueryParam;
 import com.harmonywisdom.framework.service.annotation.AutoService;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.List;
 
 public class BuildProjectAction extends BaseAction<BuildProject, BuildProjectService> {
     @AutoService
     private BuildProjectService buildProjectService;
 
+    private ProjectEIA projectEIA;
+
+    @AutoService
+    private ProjectEIAService projectEIAService;
+    @AutoService
+    private ProjectAcceptanceService projectAcceptanceService;
+    @AutoService
+    private AttachmentService attachmentService;
+
     @Override
     protected BuildProjectService getService() {
         return buildProjectService;
+    }
+
+    @Override
+    protected QueryCondition getQueryCondition() {
+        QueryParam param=new QueryParam();
+        if (StringUtils.isNotBlank(entity.getName())) {
+            param.andParam(new QueryParam("name", QueryOperator.LIKE,entity.getName()));
+        }
+        if (StringUtils.isNotBlank(entity.getBuildNature())) {
+            param.andParam(new QueryParam("buildNature", QueryOperator.EQ,entity.getBuildNature()));
+        }
+        if (StringUtils.isNotBlank(entity.getArea())) {
+            param.andParam(new QueryParam("area", QueryOperator.EQ,entity.getArea()));
+        }
+        QueryCondition condition=new QueryCondition();
+        if (param.getField()!=null) {
+            condition.setParam(param);
+        }
+        condition.setPaging(getPaging());
+        return condition;
+    }
+
+    public void listProject(){
+        getQueryCondition();
+        List<BuildProject> list=buildProjectService.getAll();
+        write(true);
+    }
+
+    public void saveHp() {
+        //获取删除的附件IDS
+        String attachmentIdsRemoveId = request.getParameter("removeId");
+        if(StringUtils.isNotBlank(attachmentIdsRemoveId)){
+            //删除附件
+            attachmentService.removeByIds(attachmentIdsRemoveId.split(","));
+        }
+        super.save();
+        if (StringUtils.isNotBlank(entity.getAttachmentIds())){
+            attachmentService.updateBusinessId(entity.getId(),entity.getAttachmentIds().split(","));
+        }
+        //环评信息保存
+        String euName=request.getParameter("euName");
+        String euTel=request.getParameter("euTel");
+        String euAddress=request.getParameter("euAddress");
+        String certificateCode=request.getParameter("certificateCode");
+        String certificateMoney=request.getParameter("certificateMoney");
+        String replyTime=request.getParameter("replyTime");
+        String replyCode=request.getParameter("replyCode");
+        String replyOrg=request.getParameter("replyOrg");
+        String isLicense=request.getParameter("isLicense");
+        String replyOpinion=request.getParameter("replyOpinion");
+        ProjectEIA projectEIA=new ProjectEIA();
+        projectEIA.setProjectId(entity.getId());
+        projectEIA.setEuName(euName);
+        projectEIA.setEuTel(euTel);
+        projectEIA.setEuAddress(euAddress);
+        projectEIA.setCertificateCode(certificateCode);
+        projectEIA.setCertificateMoney(Double.valueOf(certificateMoney));
+        projectEIA.setReplyCode(replyCode);
+        projectEIA.setReplyTime(DateUtil.strToDate(replyTime,"yyyy-MM-dd"));
+        projectEIA.setReplyOrg(replyOrg);
+        projectEIA.setIsLicense(isLicense);
+        projectEIA.setReplyOpinion(replyOpinion);
+        projectEIAService.saveOrUpdate(projectEIA);
+
+
+    }
+    public void saveYs() {
+        //获取删除的附件IDS
+        String attachmentIdsRemoveId = request.getParameter("removeId");
+        if(StringUtils.isNotBlank(attachmentIdsRemoveId)){
+            //删除附件
+            attachmentService.removeByIds(attachmentIdsRemoveId.split(","));
+        }
+        super.save();
+        if (StringUtils.isNotBlank(entity.getAttachmentIds())){
+            attachmentService.updateBusinessId(entity.getId(),entity.getAttachmentIds().split(","));
+        }
+        String replyTime=request.getParameter("replyTime");
+        String replyCode=request.getParameter("replyCode");
+        String replyOrg=request.getParameter("replyOrg");
+        String isLicense=request.getParameter("isLicense");
+        String replyOpinion=request.getParameter("replyOpinion");
+        ProjectAcceptance acceptance=new ProjectAcceptance();
+        acceptance.setProjectId(entity.getId());
+        acceptance.setReplyTime(DateUtil.strToDate(replyTime,"yyyy-MM-dd"));
+        acceptance.setReplyOrg(replyOrg);
+        acceptance.setReplyCode(replyCode);
+        acceptance.setIsLicense(isLicense);
+        acceptance.setReplyOpinion(replyOpinion);
+        projectAcceptanceService.saveOrUpdate(acceptance);
+    }
+
+    /**
+     * 删除实体时删除关联的附件
+     */
+    @Override
+    public void delete() {
+        String deleteId = request.getParameter("deletedId");
+        if(StringUtils.isNotBlank(deleteId)){
+            attachmentService.removeByBusinessIds(deleteId);
+        }
+        super.delete();
     }
 }
---
+++ src/main/webapp/container/gov/detect/scripts/buildproject.js
@@ -0,0 +1,457 @@
+var gridTable = $('#table'),
+    removeBtn = $('#remove'),
+    updateBtn = $('#update'),
+    form = $("#hpForm"),
+    ysform = $("#ysForm"),
+    formTitle = "建设项目环评及验收信息",
+    selections = [];
+
+//保存ajax请求
+function saveAjax(entity, callback) {
+    $.ajax({
+        url: rootPath + "/action/S_composite_BuildProject_saveHp.action",
+        type:"post",
+        data:entity,
+        dataType:"json",
+        success:callback
+    });
+}
+//保存ajax请求
+function saveYs(entity, callback) {
+    $.ajax({
+        url: rootPath + "/action/S_composite_BuildProject_saveYs.action",
+        type:"post",
+        data:entity,
+        dataType:"json",
+        success:callback
+    });
+}
+/**
+ * 删除请求
+ * @param ids 多个,号分隔
+ * @param callback
+ */
+function deleteAjax(ids, callback) {
+    $.ajax({
+        url: rootPath + "/action/S_composite_BuildProject_delete.action",
+        type:"post",
+        data:$.param({deletedId:ids},true),//阻止深度序列化，向后台传递数组
+        dataType:"json",
+        success:callback
+    });
+}
+/**============grid 列表初始化相关代码============**/
+function initTable() {
+    gridTable.bootstrapTable({
+        contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+        sidePagination:"server",
+        url: rootPath+"/action/S_composite_BuildProject_list.action",
+        height: pageUtils.getTableHeight(),
+        method:'post',
+        pagination:true,
+        clickToSelect:true,//单击行时checkbox选中
+        queryParams:pageUtils.localParams,
+        columns: [
+            {
+                title:"全选",
+                checkbox: true,
+                align: 'center',
+                radio:false,  //  true 单选， false多选
+                valign: 'middle'
+            },
+            {
+                title: 'ID',
+                field: 'id',
+                align: 'center',
+                valign: 'middle',
+                sortable: false,
+                visible:false
+            },
+            {
+                title: '项目名称',
+                field: 'name',
+                editable: false,
+                sortable: false,
+                align: 'center'
+            },
+            {
+                title: '行政区',
+                field: 'area',
+                sortable: false,
+                align: 'center',
+                editable: false
+            },
+            {
+                title: '批复时间',
+                field: 'replyTime',
+                sortable: false,
+                align: 'center',
+                editable: false,
+                formatter:function (value, row, index) {
+                    return pageUtils.sub10(value);
+                }
+            },
+            {
+                title: '建设性质',
+                field: 'buildNature',
+                sortable: false,
+                align: 'center',
+                editable: false,
+                formatter:function (value, row, index) {
+                    if(1==value){
+                        value="新建"
+                    }else if (2==value){
+                        value="改扩建"
+                    }else if (3==value){
+                        value="技术改造"
+                    }
+                    return value;
+                }
+            },
+            {
+                field: 'operate',
+                title: '操作',
+                align: 'center',
+                events: operateEvents,
+                formatter: operateFormatter
+            }
+        ]
+    });
+    // sometimes footer render error.
+    setTimeout(function () {
+        gridTable.bootstrapTable('resetView');
+    }, 200);
+
+    //列表checkbox选中事件
+    gridTable.on('check.bs.table uncheck.bs.table ' +
+        'check-all.bs.table uncheck-all.bs.table', function () {
+        //有选中数据，启用删除按钮
+        removeBtn.prop('disabled', !gridTable.bootstrapTable('getSelections').length);
+        //选中一条数据启用修改按钮
+        updateBtn.prop('disabled', !(gridTable.bootstrapTable('getSelections').length== 1));
+    });
+
+    $(window).resize(function () {
+        // 重新设置表的高度
+        gridTable.bootstrapTable('resetView', {
+            height: pageUtils.getTableHeight()
+        });
+    });
+}
+
+// 生成列表操作方法
+function operateFormatter(value, row, index) {
+    return '<button type="button" class="btn btn-md btn-warning view" data-toggle="modal" data-target="#hpForm">详情</button>';
+}
+// 列表操作事件
+window.operateEvents = {
+    'click .view': function (e, value, row, index) {
+        setFormView(row);
+    }
+};
+/**
+ * 获取列表所有的选中数据id
+ * @returns {*}
+ */
+function getIdSelections() {
+    return $.map(gridTable.bootstrapTable('getSelections'), function (row) {
+        return row.id
+    });
+}
+
+/**
+ *  获取列表所有的选中数据
+ * @returns {*}
+ */
+function getSelections() {
+    return $.map(gridTable.bootstrapTable('getSelections'), function (row) {
+        return row;
+    });
+}
+
+initTable();
+/**============列表工具栏处理============**/
+//初始化按钮状态
+removeBtn.prop('disabled', true);
+updateBtn.prop('disabled', true);
+/**
+ * 列表工具栏 新增和更新按钮打开form表单，并设置表单标识
+ */
+$("#add").bind('click',function () {
+    resetForm();
+});
+$("#update").bind("click",function () {
+    setFormData(getSelections()[0]);
+});
+/**
+ * 列表工具栏 删除按钮
+ */
+removeBtn.click(function () {
+    var ids = getIdSelections();
+    deleteAjax(ids,function (msg) {
+        gridTable.bootstrapTable('remove', {
+            field: 'id',
+            values: ids
+        });
+        removeBtn.prop('disabled', true);
+    });
+
+});
+
+
+
+/**============列表搜索相关处理============**/
+//搜索按钮处理
+$("#search").click(function () {
+    var queryParams = {};
+    var name = $("#s_name").val();
+    var buildNature = $("#s_buildNature").val();
+    var area = $("#s_area").val();
+    if (name){
+        queryParams["name"] = name;
+    }
+    if (buildNature){
+        queryParams["buildNature"] = buildNature;
+    }
+    if (area){
+        queryParams["area"] = area;
+    }
+    gridTable.bootstrapTable('refresh',{
+        query:queryParams
+    });
+});
+
+/**============表单初始化相关代码============**/
+
+//初始化表单验证
+var ef = form.easyform({
+    success:function (ef) {
+        var entity = $("#hpForm").find("form").formSerializeObject();
+        entity.attachmentIds = getAttachmentIds();
+        saveAjax(entity,function (msg) {
+            form.modal('hide');
+            gridTable.bootstrapTable('refresh');
+        });
+    }
+});
+//初始化表单验证
+var ys = form.easyform({
+    success:function (ef) {
+        var entity = $("#ysForm").find("form").formSerializeObject();
+        entity.attachmentIds = getAttachmentIds();
+        saveYs(entity,function (msg) {
+            form.modal('hide');
+            gridTable.bootstrapTable('refresh');
+        });
+    }
+});
+
+//表单 保存按钮
+$("#hpsave").bind('click',function () {
+    //验证表单，验证成功后触发ef.success方法保存数据
+    ef.submit(false);
+});
+//表单 保存按钮
+$("#ysSave").bind('click',function () {
+    //验证表单，验证成功后触发ef.success方法保存数据
+    ys.submit(false);
+});
+
+//初始化日期组件
+$('#replyTime1').datetimepicker({
+    language:   'zh-CN',
+    autoclose: 1,
+    minView: 2
+});
+$('#replyTime').datetimepicker({
+    language:   'zh-CN',
+    autoclose: 1,
+    minView: 2
+});
+$('#acceptTime').datetimepicker({
+    language:   'zh-CN',
+    autoclose: 1,
+    minView: 2
+});
+/**
+ * 设置表单数据
+ * @param entity
+ * @returns {boolean}
+ */
+function setFormData(entity) {
+    resetForm();
+    if (!entity) {return false}
+    form.find(".form-title").text("修改"+formTitle);
+    var id = entity.id;
+    $("#id").val(entity.id);
+    $("#removeId").val("");
+    $("#name").val(entity.name);
+    $("#EnvManagType").val(entity.EnvManagType);
+    $("#buildNature").val(entity.buildNature);
+    $("#area").val(entity.area);
+    $("#EnvInvestment").val(entity.EnvInvestment);
+    $("#industryType").val(entity.industryType);
+    $("#buildAddress").val(entity.buildAddress);
+    $("#content").val(entity.content);
+    $("#investment").val(entity.investment);
+    $("#proportion").val(entity.proportion);
+    $("#builderName").val(entity.builderName);
+    $("#builderTel").val(entity.builderTel);
+    $("#builderAddress").val(entity.builderAddress);
+    $("#builderZipCode").val(entity.builderZipCode);
+    $("#builderAP").val(entity.builderAP);
+    $("#builderLinkman").val(entity.builderLinkman);
+    $("#euName").val(entity.euName);
+    $("#euTel").val(entity.euTel);
+    $("#euAddress").val(entity.euAddress);
+    $("#certificateCode").val(entity.certificateCode);
+    $("#certificateMoney").val(entity.certificateMoney);
+    $("#replyTime").val(entity.replyTime);
+    $("#replyCode").val(entity.replyCode);
+    $("#replyOrg").val(entity.replyOrg);
+    $("#isLicense").val(entity.isLicense);
+    $("#replyOpinion").val(entity.replyOpinion);
+    $("#acceptTime").val(entity.acceptTime);
+    $("#acceptOrg").val(entity.acceptOrg);
+    $("#replyTime1").val(entity.replyTime);
+    uploader = new qq.FineUploader(getUploaderOptions(id));
+}
+function setFormView(entity) {
+    setFormData(entity);
+    form.find(".form-title").text("查看"+formTitle);
+    disabledForm(true);
+    var fuOptions = getUploaderOptions(entity.id);
+    fuOptions.callbacks.onSessionRequestComplete = function () {
+        $("#fine-uploader-gallery").find(".qq-upload-delete").hide();
+    };
+    uploader = new qq.FineUploader(fuOptions);
+    $(".qq-upload-button").hide();
+}
+function disabledForm(disabled) {
+    form.find("input").attr("disabled",disabled);
+    if (!disabled) {
+        //初始化日期组件
+        $('#replyTimeContent').datetimepicker({
+            language:   'zh-CN',
+            autoclose: 1,
+            minView: 2
+        });
+        $('#acceptTime').datetimepicker({
+            language:   'zh-CN',
+            autoclose: 1,
+            minView: 2
+        });
+    }else{
+        $('#replyTimeContent').datetimepicker('remove');
+        $('#acceptTime').datetimepicker('remove');
+
+    }
+
+}
+/**
+ * 重置表单
+ */
+function resetForm() {
+    form.find(".form-title").text("新增"+formTitle);
+    form.find("input[type!='radio'][type!='checkbox'],textarea").val("");
+    uploader = new qq.FineUploader(getUploaderOptions());
+    disabledForm(false);
+}
+
+//表单附件相关js
+var uploader;//附件上传组件对象
+/**
+ * 获取上传组件options
+ * @param bussinessId
+ * @returns options
+ */
+function getUploaderOptions(bussinessId) {
+    return {
+        element: document.getElementById("fine-uploader-gallery"),
+        template: 'qq-template',
+        chunking: {
+            enabled: false,
+            concurrent: {
+                enabled: true
+            }
+        },
+        resume: {
+            enabled: false
+        },
+        retry: {
+            enableAuto: false,
+            showButton: false
+        },
+        failedUploadTextDisplay: {
+            mode: 'custom'
+        },
+        callbacks: {
+            onComplete:function (id,fileName,msg,request) {
+                uploader.setUuid(id, msg.id);
+            },
+            onDeleteComplete:function (id) {
+                var file = uploader.getUploads({id:id});
+                var removeIds = $("#removeId").val();
+                if (removeIds) {
+                    removeIds+= ("," + file.uuid)
+                }else{
+                    removeIds = file.uuid;
+                }
+                $("#removeId").val(removeIds);
+            },
+            onAllComplete: function (succeed) {
+                var self = this;
+                $.each(succeed, function (k, v) {
+                    $('.qq-upload-download-selector', self.getItemByFileId(v)).toggleClass('qq-hide', false);
+                });
+            }
+        },
+        request: {
+            endpoint: rootPath + '/Upload',
+            params: {
+                businessId:bussinessId
+            }
+        },
+        session:{
+            endpoint: rootPath + '/action/S_attachment_Attachment_listAttachment.action',
+            params: {
+                businessId:bussinessId
+            }
+        },
+        deleteFile: {
+            enabled: true,
+            endpoint: rootPath + "/action/S_attachment_Attachment_delete.action",
+            method:"POST"
+        },
+        validation: {
+            acceptFiles: ['.jpeg', '.jpg', '.gif', '.png'],
+            allowedExtensions: ['jpeg', 'jpg', 'gif', 'png'],
+            itemLimit: 3
+        },
+        debug: true
+    };
+}
+/**
+ * 获取附件列表ids
+ * @returns {*}
+ */
+function getAttachmentIds() {
+    var attachments = uploader.getUploads();
+    if (attachments && attachments.length) {
+        var ids = [];
+        for (var i = 0 ; i < attachments.length; i++){
+            ids.push(attachments[i].uuid);
+        }
+        return ids.join(",");
+    }
+    return "";
+}
+
+/**
+ * 绑定下载按钮事件
+ */
+$("#fine-uploader-gallery").on('click', '.qq-upload-download-selector', function () {
+    var uuid = uploader.getUuid($(this.closest('li')).attr('qq-file-id'));
+    window.location.href = rootPath+"/action/S_attachment_Attachment_download.action?id=" + uuid;
+});
+
