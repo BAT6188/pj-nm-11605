0429067cf11aad583143650946f9249400e1689f
xuxiangyi <xuxiangyi@harmonywisdom.com>
2016-11-02 19:05:54 +0800

    短信发送功能后台

--- src/main/java/com/harmonywisdom/dshbcbp/common/dict/action/DictAction.java
+++ src/main/java/com/harmonywisdom/dshbcbp/common/dict/action/DictAction.java
@@ -8,7 +8,10 @@ import com.harmonywisdom.apportal.sdk.person.PersonServiceUtil;
 import com.harmonywisdom.dshbcbp.common.dict.bean.DictBean;
 import com.harmonywisdom.dshbcbp.common.dict.bean.OrgPerson;
 import com.harmonywisdom.dshbcbp.common.dict.util.DictUtil;
+import com.harmonywisdom.dshbcbp.office.bean.Contacts;
+import com.harmonywisdom.dshbcbp.office.service.ContactsService;
 import com.harmonywisdom.framework.action.ActionHelper;
+import com.harmonywisdom.framework.service.annotation.AutoService;
 import com.opensymphony.xwork2.Preparable;
 import org.apache.commons.lang.StringUtils;
 import org.apache.struts2.ServletActionContext;
@@ -22,6 +25,8 @@ import java.util.Map;
  * Created by hotleave on 15-3-9.
  */
 public class DictAction extends ActionHelper implements Preparable {
+    @AutoService
+    private ContactsService contactsService;
     /**
      * 输出数据字典项
      * <p>
@@ -82,6 +87,9 @@ public class DictAction extends ActionHelper implements Preparable {
         response = ServletActionContext.getResponse();
     }
 
+    /**
+     * 组织机构人员获取(系统发送)
+     */
     public void getOrgPersonList(){
         List<OrgPerson> orgPersonList = new ArrayList<OrgPerson>();
         String[] orgCode = getParamValues("orgCode");
@@ -99,6 +107,13 @@ public class DictAction extends ActionHelper implements Preparable {
         write(orgPersonList);
     }
 
+    /**
+     * 获取组织机构的人员
+     * @param orgCode
+     * @param orgParentId
+     * @param type
+     * @return
+     */
     public List<OrgPerson> findOrgPersonByOrgCode(String orgCode,String orgParentId,String type){
         List<OrgPerson> orgPersonList = new ArrayList<>();
 
@@ -109,8 +124,10 @@ public class DictAction extends ActionHelper implements Preparable {
         org.setName(iOrg.getOrgName());
         org.setIcon("common/images/ztree/department.png");
         orgPersonList.add(org);
+
+        List<IPerson> personList = PersonServiceUtil.getPersonByOrgId(iOrg.getOrgId());
+        List<IOrg> orgs = OrgServiceUtil.getOrgsByParentOrgId(iOrg.getOrgId());
         if(StringUtils.isNotBlank(type) && (type.equals("1") || type.equals("2"))){
-            List<IPerson> personList = PersonServiceUtil.getPersonByOrgId(iOrg.getOrgId());
             if(personList.size()>0){
                 for (IPerson iPerson:personList){
                     OrgPerson orgPerson = new OrgPerson();
@@ -129,6 +146,73 @@ public class DictAction extends ActionHelper implements Preparable {
                     orgPersonList.add(orgPerson);
                 }
             }else{
+                if(orgs.size()==0){
+                    OrgPerson noOrg = new OrgPerson();
+                    noOrg.setId("false");
+                    noOrg.setParentId(iOrg.getOrgId());
+                    noOrg.setName("没有查询到相关人员");
+                    noOrg.setIcon("common/images/ztree/remind.png");
+                    orgPersonList.add(noOrg);
+                }
+            }
+        }
+        if(StringUtils.isNotBlank(type) && (type.equals("1") || type.equals("3"))){
+            if(orgs.size()>0){
+                for(IOrg iOrgChild:orgs){
+                    List<OrgPerson> orgPersons = findOrgPersonByOrgCode(iOrgChild.getOrgCode(),iOrg.getOrgId(),"1");
+                    if(orgPersons.size()>0){
+                        for(OrgPerson op:orgPersons){
+                            orgPersonList.add(op);
+                        }
+                    }
+                }
+            }
+        }
+        return orgPersonList;
+    }
+
+    /**
+     * 获取组织机构通讯录人员
+     */
+    public void getOrgContactsList(){
+        String orgCode = getParamValue("orgCode");
+        List<OrgPerson> orgPersonList = findOrgContactsPerson(orgCode,"-1");
+        write(orgPersonList);
+    }
+    public List<OrgPerson> findOrgContactsPerson(String orgCode,String orgParentId){
+        List<OrgPerson> orgPersonList = new ArrayList<>();
+
+        IOrg iOrg = OrgServiceUtil.getOrgByOrgCode(orgCode);
+        OrgPerson org = new OrgPerson();
+        org.setId(iOrg.getOrgId());
+        org.setParentId(orgParentId);
+        org.setName(iOrg.getOrgName());
+        org.setIcon("common/images/ztree/department.png");
+        orgPersonList.add(org);
+
+        Contacts contacts = new Contacts();
+        contacts.setOrgId(iOrg.getOrgId());
+        List<Contacts> contactsList = contactsService.findBySample(contacts);
+        List<IOrg> orgs = OrgServiceUtil.getOrgsByParentOrgId(iOrg.getOrgId());
+        if(contactsList.size()>0){
+            for (Contacts c:contactsList){
+                OrgPerson orgPerson = new OrgPerson();
+                orgPerson.setId(c.getId());
+                orgPerson.setName(c.getName());
+                orgPerson.setJob(c.getPosition());
+                orgPerson.setParentId(iOrg.getOrgId());
+                orgPerson.setDepartment(c.getDepartment());
+                orgPerson.setMobilePhone(c.getPhone());
+                orgPerson.setOfficePhone(c.getTel());
+                if(StringUtils.isNotBlank(c.getSex()) && c.getSex().equals("2")){
+                    orgPerson.setIcon("common/images/ztree/female_lady_user_woman.png");
+                }else{
+                    orgPerson.setIcon("common/images/ztree/head_male_man_user.png");
+                }
+                orgPersonList.add(orgPerson);
+            }
+        }else{
+            if(orgs.size()==0){
                 OrgPerson noOrg = new OrgPerson();
                 noOrg.setId("false");
                 noOrg.setParentId(iOrg.getOrgId());
@@ -137,15 +221,12 @@ public class DictAction extends ActionHelper implements Preparable {
                 orgPersonList.add(noOrg);
             }
         }
-        if(StringUtils.isNotBlank(type) && (type.equals("1") || type.equals("3"))){
-            List<IOrg> orgs = OrgServiceUtil.getOrgsByParentOrgId(iOrg.getOrgId());
-            if(orgs.size()>0){
-                for(IOrg iOrgChild:orgs){
-                    List<OrgPerson> orgPersons = findOrgPersonByOrgCode(iOrgChild.getOrgCode(),iOrg.getOrgId(),"1");
-                    if(orgPersons.size()>0){
-                        for(OrgPerson op:orgPersons){
-                            orgPersonList.add(op);
-                        }
+        if(orgs.size()>0){
+            for(IOrg iOrgChild:orgs){
+                List<OrgPerson> orgPersons = findOrgContactsPerson(iOrgChild.getOrgCode(),iOrg.getOrgId());
+                if(orgPersons.size()>0){
+                    for(OrgPerson op:orgPersons){
+                        orgPersonList.add(op);
                     }
                 }
             }
