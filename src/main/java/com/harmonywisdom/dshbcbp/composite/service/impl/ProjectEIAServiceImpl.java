b8450885e65a8ca2c2940d1f9b108b26c7b05488
hejia <hejia@harmonywisdom.com>
2016-11-05 19:22:06 +0800

    企业环评验收修改

--- src/main/java/com/harmonywisdom/dshbcbp/composite/service/impl/ProjectEIAServiceImpl.java
+++ src/main/java/com/harmonywisdom/dshbcbp/composite/service/impl/ProjectEIAServiceImpl.java
@@ -37,20 +37,31 @@ public class ProjectEIAServiceImpl extends BaseService<ProjectEIA, String> imple
     @Override
     public List<Object[]> findByRatio(String startdate, String lastdate, String enterpriseId) {
         String whereSql = " where 1=1 ";
-        whereSql += "AND  t1.is_License = '1'";
+        whereSql += "AND  t1.IS_EIA_LICENSE = '1'";
         if(startdate != null && "".equals(startdate)){
-            whereSql += "AND DATE_FORMAT(t1.REPLY_TIME,'%Y-%m-%d') >='"+startdate+"' AND DATE_FORMAT(t1.REPLY_TIME,'%Y-%m-%d') <= '"+lastdate+"'";
+            whereSql += "AND DATE_FORMAT(t1.REPLY_EIA_TIME,'%Y-%m-%d') >='"+startdate+"' AND DATE_FORMAT(t1.REPLY_EIA_TIME,'%Y-%m-%d') <= '"+lastdate+"'";
         }else if(lastdate !=null && "".equals(lastdate)){
-            whereSql += "AND DATE_FORMAT(t1.REPLY_TIME,'%Y-%m-%d') >='"+startdate+"' AND DATE_FORMAT(t1.REPLY_TIME,'%Y-%m-%d') <= '"+lastdate+"'";
+            whereSql += "AND DATE_FORMAT(t1.REPLY_EIA_TIME,'%Y-%m-%d') >='"+startdate+"' AND DATE_FORMAT(t1.REPLY_EIA_TIME,'%Y-%m-%d') <= '"+lastdate+"'";
         }else if(enterpriseId != null && "".equals("enterpriseId")){
             whereSql += "AND a1.ENTERPRISE_ID = '"+enterpriseId+"'";
         }
         whereSql +="GROUP BY MONTH";
-        List<Object[]> list = getDAO().queryNativeSQL("SELECT DATE_FORMAT(REPLY_TIME,'%Y-%m')AS MONTH,COUNT(*) FROM HW_PROJECT_EIA t1 " +
+        List<Object[]> list = getDAO().queryNativeSQL("SELECT DATE_FORMAT(REPLY_EIA_TIME,'%Y-%m')AS MONTH,COUNT(*) FROM HW_PROJECT_EIA t1 " +
                 "LEFT JOIN HW_BUILD_PROJECT a1 ON t1.project_id=a1.id" + whereSql);
 
-        List<Object[]> list2 = getDAO().queryNativeSQL("SELECT DATE_FORMAT(REPLY_TIME,'%Y-%m')AS MONTH,COUNT(*) FROM HW_PROJECT_ACCEPTANCE t1 " +
-                "LEFT JOIN HW_BUILD_PROJECT a1 ON t1.project_id=a1.id" + whereSql);
+        String whereSql2 = " where 1=1 ";
+        whereSql2 += "AND  t1.IS_ACC_LICENSE = '1'";
+        if(startdate != null && "".equals(startdate)){
+            whereSql2 += "AND DATE_FORMAT(t1.REPLY_ACC_TIME,'%Y-%m-%d') >='"+startdate+"' AND DATE_FORMAT(t1.REPLY_ACC_TIME,'%Y-%m-%d') <= '"+lastdate+"'";
+        }else if(lastdate !=null && "".equals(lastdate)){
+            whereSql2 += "AND DATE_FORMAT(t1.REPLY_ACC_TIME,'%Y-%m-%d') >='"+startdate+"' AND DATE_FORMAT(t1.REPLY_ACC_TIME,'%Y-%m-%d') <= '"+lastdate+"'";
+        }else if(enterpriseId != null && "".equals("enterpriseId")){
+            whereSql2 += "AND a1.ENTERPRISE_ID = '"+enterpriseId+"'";
+        }
+        whereSql2 +="GROUP BY MONTH";
+
+        List<Object[]> list2 = getDAO().queryNativeSQL("SELECT DATE_FORMAT(REPLY_ACC_TIME,'%Y-%m')AS MONTH,COUNT(*) FROM HW_PROJECT_ACCEPTANCE t1 " +
+                "LEFT JOIN HW_BUILD_PROJECT a1 ON t1.project_id=a1.id" + whereSql2);
 
         List<Object[]> temp = new ArrayList<>();
 
