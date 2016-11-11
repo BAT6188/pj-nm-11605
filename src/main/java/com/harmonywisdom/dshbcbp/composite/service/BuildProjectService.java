9fb06357ee870d52901e8f55af74106402678d01
Administrator <kongxuhua@harmonywisdom.com>
2016-11-11 09:39:27 +0800

    环评验收

--- src/main/java/com/harmonywisdom/dshbcbp/composite/service/BuildProjectService.java
+++ src/main/java/com/harmonywisdom/dshbcbp/composite/service/BuildProjectService.java
@@ -1,6 +1,8 @@
 package com.harmonywisdom.dshbcbp.composite.service;
 
 import com.harmonywisdom.dshbcbp.composite.bean.BuildProject;
+import com.harmonywisdom.dshbcbp.composite.bean.ProjectAcceptance;
+import com.harmonywisdom.dshbcbp.composite.bean.ProjectEIA;
 import com.harmonywisdom.framework.service.IBaseService;
 
 import java.util.List;
@@ -9,5 +11,9 @@ public interface BuildProjectService extends IBaseService<BuildProject, String> 
 
     List<BuildProject> getAll();
 
+    List<BuildProject> findByName(String name);
+
+    List<ProjectEIA> findEIAById(String projectId);
+    List<ProjectAcceptance> findAcceptanceById(String projectId);
 
 }
