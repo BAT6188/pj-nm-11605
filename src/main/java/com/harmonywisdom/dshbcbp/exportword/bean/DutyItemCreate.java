9e4ff8f4be2a199f509ff6111256e020a7acd34d
lihaichao <lihaichao@harmonywisdom.com>
2016-11-17 20:06:47 +0800

    pageoffice查看不符合要求，创建自定义service Demo

---
+++ src/main/java/com/harmonywisdom/dshbcbp/exportword/bean/DutyItemCreate.java
@@ -0,0 +1,111 @@
+package com.harmonywisdom.dshbcbp.exportword.bean;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DutyItemCreate implements Serializable{
+	
+	private String id;
+	
+	private String dayStartTime;
+	
+	private String dayEndTime;
+	
+	private String nightStartTime;
+	
+	private String nightEndTime;
+	
+	
+	/**
+     * 日常值班信息
+     */
+    private String dutyItemInfo;
+    
+	/**
+	 * 备注
+	 */
+	private String remark;
+	
+	
+
+	public String getId() {
+		return id;
+		
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getIdForTemp(){
+		Map m = new HashMap();
+		m.put("01","一");
+		m.put("02","二");
+		m.put("03","三");
+		m.put("04","四");
+		m.put("05","五");
+		m.put("06","六");
+		m.put("07","七");
+		m.put("08","八");
+		m.put("09","九");
+		m.put("10","十");
+		m.put("11","十一");
+		m.put("12","十二");
+		if(id!=null){
+			String[] strArray = null;   
+	        strArray = id.split("-");
+	        String month=strArray[1];
+	        return (String) m.get(month);
+		}
+		return "";
+	}
+	
+	public String getDayStartTime() {
+		return dayStartTime;
+	}
+
+	public void setDayStartTime(String dayStartTime) {
+		this.dayStartTime = dayStartTime;
+	}
+
+	public String getDayEndTime() {
+		return dayEndTime;
+	}
+
+	public void setDayEndTime(String dayEndTime) {
+		this.dayEndTime = dayEndTime;
+	}
+	
+	public String getNightStartTime() {
+		return nightStartTime;
+	}
+
+	public void setNightStartTime(String nightStartTime) {
+		this.nightStartTime = nightStartTime;
+	}
+
+	public String getNightEndTime() {
+		return nightEndTime;
+	}
+
+	public void setNightEndTime(String nightEndTime) {
+		this.nightEndTime = nightEndTime;
+	}
+
+	public String getDutyItemInfo() {
+		return dutyItemInfo;
+	}
+
+	public void setDutyItemInfo(String dutyItemInfo) {
+		this.dutyItemInfo = dutyItemInfo;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+}
---
+++ src/main/java/com/harmonywisdom/dshbcbp/exportword/service/impl/DutyItemCreateServiceImpl.java
@@ -0,0 +1,64 @@
+package com.harmonywisdom.dshbcbp.exportword.service.impl;
+
+import com.harmonywisdom.dshbcbp.exportword.bean.DutyItemCreate;
+import com.harmonywisdom.framework.dao.BaseDAO;
+import com.harmonywisdom.framework.service.BaseService;
+import org.springframework.stereotype.Service;
+
+
+@Service("dutyItemCreateService")
+public class DutyItemCreateServiceImpl extends BaseService<DutyItemCreate,String> {
+	
+	/*@Autowired
+    private DutyItemDAO dutyItemDAO;*/
+
+	
+	
+	/**
+	 * 根据businessId查询所有数据
+	 */
+	@Override
+	public DutyItemCreate findById(String businessId){
+		/*DutyItemCreate entity=new DutyItemCreate();
+		String sql="select * from HW_EMSYNTHESIS_DUTY_ITEM d where d.duty_Id='"+businessId+"'";
+		List<DutyItem>list=dutyItemDAO.getListBySQL(sql);
+		Duty duty=new Duty();
+		duty=dutyDAO.findById(businessId);
+		SimpleDateFormat sdf = new SimpleDateFormat("E");
+				
+		if(duty!=null){
+			entity.setRemark(duty.getRemark());
+			entity.setId(businessId);
+			entity.setDayStartTime(duty.getDayStartTime());
+			entity.setDayEndTime(duty.getDayEndTime());
+			entity.setNightStartTime(duty.getNightEndTime());
+			entity.setNightEndTime(duty.getNightEndTime());
+		}
+		if(list!=null&&list.size()>0){
+			String dutyItemInfos="[";
+			for(DutyItem dutyItem:list){
+				if(dutyItem!=null){
+					String dutyItemInfo= JSONObject.toJSONString(dutyItem);
+					dutyItemInfo=dutyItemInfo.substring(0, dutyItemInfo.length()-1);
+					String week=",\"week\":\""+sdf.format(dutyItem.getDutyDate()).substring(2, 3)+"\"},";
+					dutyItemInfos+=dutyItemInfo+week;
+				}
+			}
+			if(!"[".equals(dutyItemInfos)){
+				dutyItemInfos=dutyItemInfos.substring(0,dutyItemInfos.length()-1);
+			}
+			dutyItemInfos+="]";
+			entity.setDutyItemInfo(dutyItemInfos);	
+		}
+		return entity;*/
+		return null;
+	}
+
+
+
+	@Override
+	protected BaseDAO<DutyItemCreate, String> getDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
