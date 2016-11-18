12ff4513e0aa183c70f51c66f21403a872998651
liuyifan <liuyifan@harmonywisdom.com>
2016-11-18 09:57:57 +0800

    创模建设信息管理

--- src/main/java/com/harmonywisdom/dshbcbp/office/dao/CreationModeDAO.java
+++
@@ -1,9 +0,0 @@
-package com.harmonywisdom.dshbcbp.office.dao;
-
-import com.harmonywisdom.dshbcbp.office.bean.CreationMode;
-import com.harmonywisdom.framework.dao.DefaultDAO;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class CreationModeDAO extends DefaultDAO<CreationMode, String> {
-}
