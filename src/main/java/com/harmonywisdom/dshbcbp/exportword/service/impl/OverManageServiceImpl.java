751c83b1d64e5c32e0392a9d2ed62b3df37b48a1
liuyifan <liuyifan@harmonywisdom.com>
2016-12-30 10:25:50 +0800

    办结管理回退用pageOffice

--- src/main/java/com/harmonywisdom/dshbcbp/exportword/service/impl/OverManageServiceImpl.java
+++ src/main/java/com/harmonywisdom/dshbcbp/exportword/service/impl/OverManageServiceImpl.java
@@ -11,16 +11,13 @@ import com.harmonywisdom.dshbcbp.enterprise.bean.Enterprise;
 import com.harmonywisdom.dshbcbp.enterprise.dao.EnterpriseDAO;
 import com.harmonywisdom.dshbcbp.exelaw.bean.Punish;
 import com.harmonywisdom.dshbcbp.exelaw.dao.PunishDAO;
-import com.harmonywisdom.dshbcbp.exportword.bean.DutyItemCreate;
 import com.harmonywisdom.dshbcbp.exportword.bean.OverManage;
 import com.harmonywisdom.framework.dao.BaseDAO;
 import com.harmonywisdom.framework.service.BaseService;
-import com.harmonywisdom.framework.service.annotation.AutoService;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 
 
