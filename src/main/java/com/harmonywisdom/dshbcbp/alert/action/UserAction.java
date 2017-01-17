baeeb48c69da79f7b3562a6f4d6ef9e32bcb3ee0
liuyifan <liuyifan@harmonywisdom.com>
2017-01-17 14:43:23 +0800

    手机端发布信息之前验证登陆信息

--- src/main/java/com/harmonywisdom/dshbcbp/alert/action/UserAction.java
+++ src/main/java/com/harmonywisdom/dshbcbp/alert/action/UserAction.java
@@ -1,10 +1,17 @@
 package com.harmonywisdom.dshbcbp.alert.action;
 
+import com.harmonywisdom.apportal.sdk.org.IOrg;
+import com.harmonywisdom.apportal.sdk.org.OrgServiceUtil;
+import com.harmonywisdom.apportal.sdk.person.IPerson;
 import com.harmonywisdom.apportal.sdk.person.PersonServiceUtil;
 import com.harmonywisdom.core.user.IUserProfile;
 import com.harmonywisdom.core.user.impl.UserProfile;
 import com.harmonywisdom.framework.action.BaseAction;
 import com.harmonywisdom.framework.service.IBaseService;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by Administrator on 2016/12/16.
@@ -13,6 +20,25 @@ public class UserAction extends BaseAction {
     @Override
     protected IBaseService getService() {
         return null;
+    }
+
+    /**
+     * 手机端发布信息之前验证登陆信息
+     */
+    public void loginValidate(){
+        String userId = request.getParameter("userId");
+        String pwd = request.getParameter("password");
+        if (PersonServiceUtil.validateUserPassword(userId, pwd)){
+            IPerson person = PersonServiceUtil.getPersonByUserId(userId);
+            IOrg org = OrgServiceUtil.getOrgByPersonId(person.getPersonId());
+            Map<String, Object> ret = new HashMap<String, Object>();
+            ret.put("person", person);
+            ret.put("org", org);
+            write(ret);
+        }else{
+            write(false);
+        }
+
     }
 
     public void govUpdatePwd(){
