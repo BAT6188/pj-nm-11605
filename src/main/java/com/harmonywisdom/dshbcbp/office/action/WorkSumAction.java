2d4a65953d7dcbe6fd1b85116a55001a158f70db
liuyifan <liuyifan@harmonywisdom.com>
2017-02-04 20:06:46 +0800

    bug修改

--- src/main/java/com/harmonywisdom/dshbcbp/office/action/WorkSumAction.java
+++ src/main/java/com/harmonywisdom/dshbcbp/office/action/WorkSumAction.java
@@ -94,10 +94,11 @@ public class WorkSumAction extends BaseAction<WorkSum, WorkSumService> {
             attachmentService.removeByIds(attachmentIdsRemoveId.split(","));
         }
         entity.setPubTime(new Date());
+        super.save();
         if (StringUtils.isNotBlank(entity.getAttachmentIds())){
             attachmentService.updateBusinessId(entity.getId(),entity.getAttachmentIds().split(","));
         }
-        super.save();
+
     }
 
     /**
