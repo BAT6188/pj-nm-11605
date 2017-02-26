3750e61d0615d71a0d143cc74530dc1eecf3f62b
xuxiangyi <xuxiangyi@harmonywisdom.com>
2017-02-26 16:07:07 +0800

    port时间戳

--- src/main/java/com/harmonywisdom/dshbcbp/port/bean/TransportEfficient.java
+++ src/main/java/com/harmonywisdom/dshbcbp/port/bean/TransportEfficient.java
@@ -92,7 +92,8 @@ public class TransportEfficient implements Serializable {
     /**
      * `mobile_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
      */
-    @Column(name = "mobile_timestamp")
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "mobile_timestamp",nullable = false,columnDefinition = "CURRENT_TIMESTAMP")
     private Date mobileTimestamp;
 
 
